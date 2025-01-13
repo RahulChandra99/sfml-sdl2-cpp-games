c651175d02faba8e69109be82c160b72ec633759
Rahul Chandra <rahulchandra99@gmail.com>
2025-01-13 20:34:37 +0000

    sfml project push

---
+++ SpaceHero-sfml/SpaceHero/src/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <SFML/Graphics.hpp>
+
+int main()
+{
+	sf::RenderWindow renderWindow{ sf::VideoMode(100,100), "My Window" };
+	std::cout << "Hello Worldss";
+}
---
+++ SpaceHero-sfml/build/CMakeFiles/fc-tmp/sfml/update.cmake
@@ -0,0 +1,9 @@
+cmake_minimum_required(VERSION ${CMAKE_VERSION}) # this file comes with cmake
+
+message(VERBOSE "Executing update step for sfml")
+
+block(SCOPE_FOR VARIABLES)
+
+include("D:/GameDevelopment/Cpp/SpaceHero/build/CMakeFiles/fc-tmp/sfml/sfml-gitupdate.cmake")
+
+endblock()
